760a405f16718f8a932f5257b645d8b47b4ae7e9
Jordomav <jordomav@gmail.com>
2016-02-27 09:59:06 -0800

    Prompt Box added

--- dialog.js
+++ dialog.js
@@ -27,7 +27,6 @@ var Alert = new CustomAlert();
 //Start Confirm Dialogue box
 function deletePost(id){
     var db_id = id.replace("post_", "");
-    // Run Ajax request here to delete post from database
     document.body.removeChild(document.getElementById(id));
 }
 function CustomConfirm(){
@@ -61,5 +60,32 @@ function CustomConfirm(){
 var Confirm = new CustomConfirm();
 //End Confirm Dialogue Box
 //Start Prompt Box
-
+function CustomPrompt(){
+    this.render = function(dialog,func){
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxhead').innerHTML = "A value is required";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+        document.getElementById('dialogboxbody').innerHTML += '<br><input id="prompt_value1">';
+        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Prompt.ok(\''+func+'\')">OK</button> <button onclick="Prompt.cancel()">Cancel</button>';
+    }
+    this.cancel = function(){
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+    this.ok = function(func){
+        var prompt_value1 = document.getElementById('prompt_value1').value;
+        window[func](prompt_value1);
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+}
+var Prompt = new CustomPrompt();
 //End Prompt Box
